e8804f10c7940d92d8c9e4bf40908fb3aa242372
Rui Yang <ruiyang@Ruis-MacBook-Air.local>
2017-01-31 19:05:32 -0500

    Update route registering

--- route.php
+++ route.php
@@ -1,8 +1,12 @@
 <?php
 
-$viewPath = view_path();
+/*
+ |-----------------------------------------------------
+ | Register routes for the application.
+ |-----------------------------------------------------
+ */
 
-route_register(['method' => 'GET', 'uri' => '/'], $viewPath.DIRECTORY_SEPARATOR.'home.php');
-route_register(['method' => ['GET', 'POST'], 'uri' => '/event'], $viewPath.DIRECTORY_SEPARATOR.'event.php');
-route_register(['method' => ['GET', 'POST'], 'uri' => '/booking'], $viewPath.DIRECTORY_SEPARATOR.'booking.php');
-route_register(['method' =>'GET', 'uri' => '/purchase'], $viewPath.DIRECTORY_SEPARATOR.'purchase.php');
+route_register(['method' => 'GET', 'uri' => '/'], view_path('home.php'));
+route_register(['method' => ['GET', 'POST'], 'uri' => '/event'], view_path('event.php'));
+route_register(['method' => ['GET', 'POST'], 'uri' => '/booking'], view_path('booking.php'));
+route_register(['method' =>'GET', 'uri' => '/purchase'], view_path('purchase.php'));
